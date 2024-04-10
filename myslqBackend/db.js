966e43a8ff0c7cd8bcc3a5c8fa24740ae4d6aa5a
lucasdsr <lucasSantana.devl@gmail.com>
2024-04-10 19:54:07 -0300

    add: delete, post and patch operations

--- myslqBackend/db.js
+++ myslqBackend/db.js
@@ -8,7 +8,7 @@ const client = mysql.createPool({
     password: process.env.DB_PASSWORD,
 });
 
-// const client = mysql.createPool(process.env.CONNECTION_STRING);
+// USUARIOS -----------------------------------------------------------------------------------
 
 async function selectUsuarios() {
     const res = await client.query('SELECT * FROM usuario');
@@ -20,6 +20,24 @@ async function selectUsuarioById(id) {
     return res[0];
 }
 
+async function insertUsuario(usuario) {
+    const sql = 'INSERT INTO usuario(nome,email,password) VALUES (?,?,?);';
+    const values = [usuario.nome, usuario.email, usuario.senha];
+    await client.query(sql, values);
+}
+
+async function updateUsuario(id, usuario) {
+    const sql = 'UPDATE usuario SET nome=?, email=?, password=? WHERE id=?';
+    const values = [usuario.nome, usuario.email, usuario.password, id];
+    await client.query(sql, values);
+}
+
+async function deleteUsuario(id) {
+    return await client.query('DELETE FROM usuario where id=?;', [id]);
+}
+
+// ATIVIDADES ---------------------------------------------------------------------------------
+
 async function selectAtividades() {
     const res = await client.query('SELECT * FROM atividade');
     return res[0];
@@ -30,6 +48,24 @@ async function selectAtividadeById(id) {
     return res[0];
 }
 
+async function insertAtividade(atividade) {
+    const sql = 'INSERT INTO atividade(titulo,descricao,nota,data-limite) VALUES (?,?,?,?);';
+    const values = [atividade.titulo, atividade.descricao, atividade.nota, atividade.data-limite];
+    await client.query(sql, values);
+}
+
+async function updateAtividade(id, atividade) {
+    const sql = 'UPDATE atividade SET titulo=?, descricao=?, data-limite=? WHERE id=?';
+    const values = [atividade.titulo, atividade.descricao, atividade.data-limite, id];
+    await client.query(sql, values);
+}
+
+async function deleteAtividade(id) {
+    return await client.query('DELETE FROM atividade where id=?;', [id]);
+}
+
+// USUARIO-ATIVIDADE -------------------------------------------------------------------------
+
 async function selectUsuariosAtividades() {
     const res = await client.query('SELECT * FROM usuario_atividade');
     return res[0];
@@ -39,12 +75,34 @@ async function selectUsuarioAtividadeById(id) {
     const res = await client.query('SELECT * FROM usuario_atividade WHERE ID=?', [id]);
     return res[0];
 }
- 
+
+async function insertUsuarioAtividade(usuarioAtividade) {
+    const sql = 'INSERT INTO usuario_atividade(usuario_id,atividade_id,entrega,nota) VALUES (?,?,?,?);';
+    const values = [usuarioAtividade.usuario_id, usuarioAtividade.atividade_id, usuarioAtividade.entrega, usuarioAtividade.nota];
+    await client.query(sql, values);
+}
+
+// async function updateUsuarioAtividade(id, usuario) {
+//     const sql = 'UPDATE usuario_atividade SET nome=?, email=?, password=? WHERE id=?';
+//     const values = [usuario.nome, usuario.email, usuario.password, id];
+//     await client.query(sql, values);
+// }
+
+async function deleteUsuarioAtividade(id) {
+    return await client.query('DELETE FROM usuario_atividade where id=?;', [id]);
+}
+
 module.exports = {
     selectUsuarios,
     selectUsuarioById,
+    insertUsuario,
+    deleteUsuario,
     selectAtividades,
     selectAtividadeById,
+    insertAtividade,
+    deleteAtividade,
     selectUsuariosAtividades,
-    selectUsuarioAtividadeById
+    selectUsuarioAtividadeById,
+    insertUsuarioAtividade,
+    deleteUsuarioAtividade
 }
--- myslqBackend/index.js
+++ myslqBackend/index.js
@@ -21,6 +21,21 @@ app.get('/usuario/:id', async (req, res) => {
   res.json(results);
 })
 
+app.delete('/usuario/:id', async (req, res) => {
+  await db.deleteUsuario(req.params.id);
+  res.sendStatus(204);
+})
+
+app.post('/usuario', async (req, res) => {
+  await db.insertUsuario(req.body);
+  res.sendStatus(201);
+});
+
+app.patch('/usuario/:id', async (req, res) => {
+  await db.updateUsuario(req.params.id, req.body);
+  res.sendStatus(200);
+})
+
 app.get('/usuarios', async (req, res) => {
     const results = await db.selectUsuarios();
     res.json(results);
@@ -34,6 +49,21 @@ app.get('/atividade/:id', async (req, res) => {
   res.json(results);
 })
 
+app.delete('/atividade/:id', async (req, res) => {
+  await db.deleteAtividade(req.params.id);
+  res.sendStatus(204);
+})
+
+app.post('/atividade', async (req, res) => {
+  await db.insertAtividade(req.body);
+  res.sendStatus(201);
+});
+
+app.patch('/atividade/:id', async (req, res) => {
+  await db.updateAtividade(req.params.id, req.body);
+  res.sendStatus(200);
+})
+
 app.get('/atividades', async (req, res) => {
   const results = await db.selectAtividades();
   res.json(results);
@@ -45,6 +75,21 @@ app.get('/atividades', async (req, res) => {
 app.get('/usuario-atividade/:id', async (req, res) => {
   const results = await db.selectUsuarioAtividadeById(req.params.id);
   res.json(results);
+})
+
+app.delete('/usuario-atividade/:id', async (req, res) => {
+  await db.deleteUsuarioAtividade(req.params.id);
+  res.sendStatus(204);
+})
+
+app.post('/usuario-atividade', async (req, res) => {
+  await db.insertUsuarioAtividade(req.body);
+  res.sendStatus(201);
+});
+
+app.patch('/usuario-atividade/:id', async (req, res) => {
+  await db.updateUsuarioAtividade(req.params.id, req.body);
+  res.sendStatus(200);
 })
 
 app.get('/usuarios-atividades', async (req, res) => {
